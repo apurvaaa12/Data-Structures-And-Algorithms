7ab55d0a60b8e53976b8733455aa0058e80f6a1a
unknown <apurva.rajashekaruni7@gmail.com>
2023-04-08 21:03:04 +0530

    Arrays

--- Array/src/Date.java
+++ Array/src/Date.java
@@ -12,6 +12,9 @@ public class Date {
         Stream<String> stream= Stream.of("Jello,making me a monster");
         stream.forEach(System.out::println);
 
+        Stream<String> stream1= Stream.of("Jello,making me a monster");
+        stream1.forEach(System.out::println);
+        
         String str = "apurva";
         String str0 = "apurva";
         String str2= new String("apurva");
